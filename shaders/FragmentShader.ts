128d48926126a01acf18751410d1711da58e294e
Adam Risberg <risbergadam@gmail.com>
2016-08-21 14:33:11 +0200

    no

--- shaders/FragmentShader.ts
+++ shaders/FragmentShader.ts
@@ -4,13 +4,14 @@ precision mediump float;
 
 in vec4 vColor;
 in vec4 vNormal;
-in vec4 vPosition;
+in vec3 vPosition;
+in vec3 center;
 
 out vec4 fColor;
 
 float calcSpecular(vec3 ligth) {
     vec3 norm = vec3(vNormal);
-    vec3 camera = normalize(vec3(vPosition));
+    vec3 camera = normalize(vPosition);
     vec3 ligthReflected = reflect(ligth, norm);
 
     float diffuse = max(0.0, dot(norm, ligth));
@@ -23,9 +24,20 @@ void main(void) {
     float spec2 = calcSpecular(normalize(vec3(-0.5, 0.3, 0.2)));
     float spec3 = calcSpecular(normalize(vec3(0.5, 0.3, 0.2)));
 
+    float dist = distance(center, vPosition);
+    float heat = pow(1.0 / (dist + 1.0) * 1.2, 10.0) * 5.0;
+
+    vec3 surfaceToCenter = normalize(center - vPosition);
+    vec3 cameraToSurface = normalize(vPosition);
+    vec3 negNormal = vec3(vNormal) * -1.0;
+    cameraToSurface = cameraToSurface * 0.5 + negNormal * 0.5;
+
+    float heat2 = pow(max(0.0, dot(surfaceToCenter, cameraToSurface)) * 0.6, 4.0) + heat * 0.2;
+
     float specular = spec1 + spec2 + spec3 + 0.005;
 
-    fColor = vec4(specular, specular, specular, 1.0);
+    //fColor = vec4(specular + heat2, specular + heat2 * 0.4, specular + heat2 * 0.1, 1.0);
+    fColor = vec4(specular + heat2 * vColor.r, specular + heat2 * vColor.g, specular + heat2 * vColor.b, 1.0);
 }
 
 `
--- shaders/VertexShader.ts
+++ shaders/VertexShader.ts
@@ -12,19 +12,22 @@ uniform mat4 ProjectionView;
 
 out vec4 vColor;
 out vec4 vNormal;
-out vec4 vPosition;
-
+out vec3 vPosition;
+out vec3 center;
 void main(void) {
     if (isInstance == 0) {
         gl_Position = ProjectionView * Model * vec4(position, 1.0);
         vNormal = transpose(inverse(Model)) * vec4(normal, 1.0);
+        center = vec3(Model[3]);
+        vPosition = vec3(Model * vec4(position, 1.0));
     } else {
         gl_Position = ProjectionView * ModelInstance[gl_InstanceID] * vec4(position, 1.0);
         vNormal =  transpose(inverse(ModelInstance[gl_InstanceID])) * vec4(normal, 1.0);
+        center = vec3(ModelInstance[gl_InstanceID][3]);
+        vPosition = vec3(ModelInstance[gl_InstanceID] * vec4(position, 1.0));
     }
 
     vColor = vec4(color, 1.0);
-    vPosition = gl_Position;
 }
 
 `
